e1da40cc3af372cbbc4b56fd11dbb62b02399331
Saloni-Jenekar <salonijenekar5@gmail.com>
2025-09-26 13:31:25 +0530

    navbar

--- app.js
+++ app.js
@@ -133,5 +133,5 @@ app.use((err,req,res,next)=>{
   //res.status(statusCode).send(message);
 })
 app.listen(8080,()=>{
-    console.log("sever is listing to port 8080");
+    console.log("server is listening to port 8080");
 })
